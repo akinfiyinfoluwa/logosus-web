a57b434256dc089cb8a8955e1fcc737ddb4cc51d
akinfiyinfoluwa <apostleofwords@gmail.com>
2025-08-10 00:36:48 +0100

    updated the get function

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,11 +1,12 @@
 import { NextResponse } from 'next/server';
 import { db } from '@/database';
 import { blog } from '@/database/schema';
+import { eq } from 'drizzle-orm';
 
 export async function GET() {
   try {
     // Fetch all blog posts from the database
-    const posts = await db.select().from(blog);
+    const posts = await db.select().from(blog).where(eq(blog.published, true));
 
     return NextResponse.json({ success: true, posts }, { status: 200 });
   } catch (error) {
